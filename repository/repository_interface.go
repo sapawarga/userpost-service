ff5572e3e74b33d18eeb1ca3f20d8ca8f2cd3e6f
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-04-05 14:25:46 +0700

    #3 add unit test for get list user post

--- repository/repository_interface.go
+++ repository/repository_interface.go
@@ -10,7 +10,10 @@ type PostI interface {
 	GetListPost(ctx context.Context, request *model.UserPostRequest) ([]*model.PostResponse, error)
 	GetMetadataPost(ctx context.Context, request *model.UserPostRequest) (*int64, error)
 	GetActor(ctx context.Context, id int64) (*model.UserResponse, error)
+	GetIsLikedByUser(ctx context.Context, req *model.IsLikedByUser) (bool, error)
+}
+
+type CommentI interface {
 	GetLastComment(ctx context.Context, id int64) (*model.CommentResponse, error)
 	GetTotalComments(ctx context.Context, userPostID int64) (*int64, error)
-	GetIsLikedByUser(ctx context.Context, req *model.IsLikedByUser) (bool, error)
 }
