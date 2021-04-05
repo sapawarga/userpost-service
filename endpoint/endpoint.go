a73e8fd4b86536519b4bab6b49b9d4c272bc8be7
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-04-05 15:26:50 +0700

    #1 add endpoint for get list user post

---
+++ endpoint/endpoint.go
@@ -0,0 +1,36 @@
+package endpoint
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/sapawarga/userpost-service/model"
+	"github.com/sapawarga/userpost-service/usecase"
+)
+
+func MakeGetListUserPost(ctx context.Context, usecase usecase.UsecaseI) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(*GetListUserPostRequest)
+		resp, err := usecase.GetListPost(ctx, &model.GetListRequest{
+			ActivityName: req.ActivityName,
+			Username:     req.Username,
+			Category:     req.Category,
+			Status:       req.Status,
+			Page:         req.Page,
+			Limit:        req.Limit,
+			SortBy:       req.SortBy,
+			OrderBy:      req.OrderBy,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return &UserPostWithMetadata{
+			Data: resp.Data,
+			Metadata: &Metadata{
+				Page:      resp.Metadata.Page,
+				TotalPage: resp.Metadata.TotalPage,
+				Total:     resp.Metadata.Total,
+			},
+		}, nil
+	}
+}
---
+++ endpoint/request.go
@@ -0,0 +1,12 @@
+package endpoint
+
+type GetListUserPostRequest struct {
+	ActivityName *string `httpquery:"activity_name"`
+	Username     *string `httpquery:"username"`
+	Category     *string `httpquery:"category"`
+	Status       *int64  `httpquery:"status"`
+	Page         *int64  `httpquery:"page"`
+	Limit        *int64  `httpquery:"limit"`
+	SortBy       *string `httpquery:"sort_by"`
+	OrderBy      *string `httpquery:"order_by"`
+}
