00a072cc339aa205fd604d491b2d58735d78e39e
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-04-12 10:26:04 +0700

    #7 #3 add unit test for update detail of status or description

---
+++ mocks/testcases/update_status_or_title.go
@@ -0,0 +1,66 @@
+package testcases
+
+import (
+	"errors"
+
+	"github.com/sapawarga/userpost-service/helper"
+	"github.com/sapawarga/userpost-service/model"
+)
+
+var updateUserPost = &model.UpdatePostRequest{
+	ID:     1,
+	Status: helper.SetPointerInt64(helper.ACTIVED),
+	Title:  helper.SetPointerString("Update Description"),
+}
+
+type UpdateUserPostDetail struct {
+	Description                string
+	UsecaseRequest             *model.UpdatePostRequest
+	GetDetailParam             int64
+	UpdateUserPostParam        *model.UpdatePostRequest
+	ResponseGetDetailRepo      ResponseGetDetailUserPost
+	ResponseUpdateUserPostRepo error
+	ResponseUsecase            error
+}
+
+var UpdateUserPostDetailData = []UpdateUserPostDetail{
+	{
+		Description:         "succes_update_detail",
+		UsecaseRequest:      updateUserPost,
+		GetDetailParam:      1,
+		UpdateUserPostParam: updateUserPost,
+		ResponseGetDetailRepo: ResponseGetDetailUserPost{
+			Error: nil,
+		},
+		ResponseUpdateUserPostRepo: nil,
+		ResponseUsecase:            nil,
+	}, {
+		Description:         "failed_get_detail",
+		UsecaseRequest:      updateUserPost,
+		GetDetailParam:      1,
+		UpdateUserPostParam: updateUserPost,
+		ResponseGetDetailRepo: ResponseGetDetailUserPost{
+			Error: errors.New("failed_get_detail"),
+		},
+		ResponseUpdateUserPostRepo: nil,
+		ResponseUsecase:            errors.New("failed_get_detail"),
+	}, {
+		Description:         "failed_update_detail",
+		UsecaseRequest:      updateUserPost,
+		GetDetailParam:      1,
+		UpdateUserPostParam: updateUserPost,
+		ResponseGetDetailRepo: ResponseGetDetailUserPost{
+			Error: nil,
+		},
+		ResponseUpdateUserPostRepo: errors.New("failed_update_detail"),
+		ResponseUsecase:            errors.New("failed_update_detail"),
+	},
+}
+
+func UpdateUserPostDetailDescription() []string {
+	var arr = []string{}
+	for _, data := range UpdateUserPostDetailData {
+		arr = append(arr, data.Description)
+	}
+	return arr
+}
