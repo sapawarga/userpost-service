6ac2872b3c05fc3ce2610fbf4b4f18817f018021
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-04-21 11:05:44 +0700

    #26 add transport and proto of create comment

--- transport/grpc/handler.go
+++ transport/grpc/handler.go
@@ -8,11 +8,12 @@ import (
 )
 
 type grpcServer struct {
-	userPostGetList     kitgrpc.Handler
-	userPostGetDetail   kitgrpc.Handler
-	userPostCreateNew   kitgrpc.Handler
-	userpostUpdate      kitgrpc.Handler
-	userPostGetComments kitgrpc.Handler
+	userPostGetList       kitgrpc.Handler
+	userPostGetDetail     kitgrpc.Handler
+	userPostCreateNew     kitgrpc.Handler
+	userpostUpdate        kitgrpc.Handler
+	userPostGetComments   kitgrpc.Handler
+	userPostCreateComment kitgrpc.Handler
 }
 
 func (g *grpcServer) GetListUserPost(ctx context.Context, in *transportUserPost.GetListUserPostRequest) (*transportUserPost.GetListUserPostResponse, error) {
@@ -53,4 +54,12 @@ func (g *grpcServer) GetCommentsByID(ctx context.Context, in *transportUserPost.
 		return nil, err
 	}
 	return resp.(*transportUserPost.CommentsResponse), nil
+}
+
+func (g *grpcServer) CreateComment(ctx context.Context, in *transportUserPost.CreateCommentRequest) (*transportUserPost.StatusResponse, error) {
+	_, resp, err := g.userPostCreateComment.ServeGRPC(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*transportUserPost.StatusResponse), nil
 }
