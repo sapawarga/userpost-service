f928a155485cd382bc96b9f0234635bb0393fa7c
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-04-29 11:27:51 +0700

    #19 add function for check is the user post is liked by who login

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -122,6 +122,7 @@ func (p *Post) GetListPostByMe(ctx context.Context, params *model.GetListRequest
 
 func (p *Post) GetDetailPost(ctx context.Context, id int64) (*model.UserPostResponse, error) {
 	logger := kitlog.With(p.logger, "method", "GetDetailPost")
+	actor := ctx.Value(helper.ACTORKEY).(*model.ActorFromContext).Data
 	resp, err := p.repoPost.GetDetailPost(ctx, id)
 	if err != nil {
 		level.Error(logger).Log("error_get_detail", err)
@@ -132,6 +133,20 @@ func (p *Post) GetDetailPost(ctx context.Context, id int64) (*model.UserPostResp
 	if err != nil {
 		level.Error(logger).Log("error_get_detail_user_post", err)
 		return nil, err
+	}
+
+	isLiked, err := p.repoPost.CheckIsExistLikeOnPostBy(ctx, &model.AddOrRemoveLikeOnPostRequest{
+		UserPostID: id,
+		ActorID:    actor["id"].(int64),
+		TypeEntity: helper.TYPE_USERPOST,
+	})
+	if err != nil {
+		level.Error(logger).Log("error_check_isliked", err)
+		return nil, err
+	}
+
+	if isLiked {
+		userPost.IsLiked = isLiked
 	}
 
 	return userPost, nil
@@ -288,9 +303,6 @@ func (p *Post) getDetailOfUserPost(ctx context.Context, post *model.PostResponse
 		userPost.Actor = user
 	}
 
-	// TODO get isLiked by who is user login. Get the user login from context
-	// isLiked, err := p.repo.GetIsLikedByUser()
-
 	return userPost, nil
 }
 
@@ -319,10 +331,22 @@ func (p *Post) getDetailComment(ctx context.Context, comment *model.CommentRespo
 }
 
 func (p *Post) appendListUserPost(ctx context.Context, resp []*model.PostResponse) (userPosts []*model.UserPostResponse, err error) {
+	actorID := ctx.Value(helper.ACTORKEY).(*model.ActorFromContext).Get("id").(int64)
 	for _, v := range resp {
 		userPost, err := p.getDetailOfUserPost(ctx, v)
 		if err != nil {
 			return nil, err
+		}
+		isLiked, err := p.repoPost.CheckIsExistLikeOnPostBy(ctx, &model.AddOrRemoveLikeOnPostRequest{
+			UserPostID: v.ID,
+			ActorID:    actorID,
+			TypeEntity: helper.TYPE_USERPOST,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if isLiked {
+			userPost.IsLiked = isLiked
 		}
 		userPosts = append(userPosts, userPost)
 	}
