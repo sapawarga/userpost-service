8340d6bba7552c85ef90289d46d57d35bc72a02d
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-05-20 12:04:00 +0700

    #41 update helper for convert string to int64

--- helper/constant.go
+++ helper/constant.go
@@ -7,12 +7,17 @@ const (
 	DELETED_STRING         = "deleted"
 	INACTIVED_STRING       = "inactived"
 	ACTIVED_STRING         = "actived"
-	STATUSCREATED          = "status_created"
-	STATUSUPDATED          = "status_updated"
-	STATUSDELETED          = "status_deleted"
 	SELECT_QUERY           = "select"
 	UPDATE_QUERY           = "update"
 	TYPE_USERPOST          = "user_post"
+	HTTP_GET               = "GET"
+	HTTP_PUT               = "PUT"
+	HTTP_POST              = "POST"
+	HTTP_DELETE            = "DELETE"
+	STATUS_OK              = "status_ok"
+	STATUS_CREATED         = "status_created"
+	STATUS_UPDATED         = "status_updated"
+	STATUS_DELETED         = "status_deleted"
 )
 
 type keyType string
